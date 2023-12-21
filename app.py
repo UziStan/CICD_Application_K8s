ea4e8195663f3dcc856b6b07dda5827de70860ba
UziStan <62811668+UziStan@users.noreply.github.com>
2023-12-21 14:46:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():
 <center>
 <h1>Demo on GitOps with ArgoCD and Github Actions. BY Uzi v1</h1> <br>
 <br>
-<img src="https://github.com/tanmaybhandge/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
+<img src="https://github.com/UziStan/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
 </center>
 </body>
 </html>
